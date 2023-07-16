a00d3b1f1ce28bdd55b8a092cffb2eea88c5648b
Nafiz-Anam <121781159+Nafiz-PureTech@users.noreply.github.com>
2023-07-16 19:11:23 +0600

    code cleanup

--- routes/JobRoute/JobRoute.js
+++ routes/JobRoute/JobRoute.js
@@ -1,9 +1,5 @@
 const router = require("express").Router();
 const JobController = require("../../controller/jobController");
-const accountValidation = require("../../utilities/validations/accountValidation");
-const checkUserToken = require("../../utilities/tokenmanager/checkUserToken");
-const receiptUpload = require("../../uploads/receiptUpload");
-const KycUpload = require("../../uploads/kyc_uploader");
 const jobUploader = require("../../uploads/jobUploader");
 
 router.post("/create", jobUploader, JobController.create);
--- routes/Router.js
+++ routes/Router.js
@@ -1,11 +1,8 @@
 const router = require("express").Router();
 const UserRouter = require("./UserRoute/UserRouter");
-const AccountRouter = require("./AccountRoute/AccountRoute");
 const JobRouter = require("./JobRoute/JobRoute");
-
 
 router.use("/user", UserRouter);
 router.use("/job", JobRouter);
-
 
 module.exports = router;
